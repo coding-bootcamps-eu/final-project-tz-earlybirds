608a9677e6cd2a02b8f4f66a827510b0db2fbb66
Maike Henze <maike.henze@gmail.com>
2023-11-20 14:13:31 +0100

    adds api

---
+++ src/views/RecipesView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home"></div>
+  <ul>
+    <li v-for="recipe in recipes" :key="recipe.id">{{ recipe.title }}</li>
+  </ul>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "RecipesView",
+  components: {},
+  data() {
+    return {
+      recipes: [],
+    };
+  },
+  created() {
+    this.fetchRecipes();
+  },
+  methods: {
+    async fetchRecipes() {
+      const response = await fetch("http://localhost:3017/recipes");
+      const data = await response.json();
+      this.recipes = data;
+    },
+  },
+};
+</script>
