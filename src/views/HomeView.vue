919734b8e726b0da8378916f13b302d1e0787141
Maike Henze <maike.henze@gmail.com>
2023-11-28 13:31:53 +0100

    adds views

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home"></div>
+  <ul>
+    <li v-for="recipe in recipes" :key="recipe.id">{{ recipe.title }}</li>
+  </ul>
+</template>
+
+<script>
+// @ is an alias to /src
+</script>
---
+++ src/views/RecipeInputView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="recipe-input">
+    <form>
+      <label for="recipename">Name</label>
+      <input type="text" />
+      <p>
+        Jedes gute Gericht besteht aus verschiedenen Bausteinen, die zusammen
+        eine tolle Familienmahlzeit bilden, dazu brauchst du:
+      </p>
+      <label for="kraftpaket">Kraftpaket</label>
+      <input type="text" id="kraftpaket" name="kraftpaket" />
+      <button>Info</button>
+      <label for="denkfutter">Denkfutter</label>
+      <input type="text" id="denkfutter" name="denkfutter" />
+      <button>Info</button>
+      <label for="buntes">Buntes Allerlei</label>
+      <input type="text" id="buntes" name="buntes" />
+      <button>Info</button>
+      <label for="infos">Zusatzinfos</label>
+      <input type="text" id="infos" name="infos" />
+      <button>Info</button>
+    </form>
+  </div>
+  <ul>
+    <li v-for="recipe in recipes" :key="recipe.id">{{ recipe.title }}</li>
+  </ul>
+</template>
+
+<script>
+// @ is an alias to /src
+</script>
